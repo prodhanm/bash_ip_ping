91a522efeb2022dbb06c6c34d1d2c9e475e6491c
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-04 02:12:35 -0400

    modified the for loop to be conventional.

--- get_ip.sh
+++ get_ip.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-for ip in {1..254}
-do 
-        ping -c 1 192.168.1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done
+for (( ip=1; ip<255; ip++ ))
+	do 
+		ping -c 1 192.168.1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+	done
