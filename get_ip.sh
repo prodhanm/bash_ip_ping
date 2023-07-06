d7654e128103cf3dc9fe23e90a2e26c9ca596f23
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-05 20:15:13 -0400

    modified script to conceal ip_prefix.

--- get_ip.sh
+++ get_ip.sh
@@ -2,5 +2,5 @@
 
 for (( ip=1; ip<255; ip++ ))
 	do 
-		ping -c 1 192.168.1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+		ping -c 1 ip_prefix.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 	done
