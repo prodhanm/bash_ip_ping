df92b300c6eb9d81ab5d07915ddca1d4600276af
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-13 14:26:11 -0400

    created a ip_prefix variable.

--- get_ip.sh
+++ get_ip.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
+ip_prefix=<input ip prefix>
+
 for (( ip=1; ip<255; ip++ ))
 	do 
-		ping -c 1 ip_prefix.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+	        ping -c 1 $ip_prefix.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
 	done
